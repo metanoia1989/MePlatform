7d5b4326ef865457645a5dcaf5b89da2c9542804
metanoia1989 <sogaxili@gmail.com>
2021-06-07 03:18:17 +0800

    feat: 添加tabbar图标

---
+++ frontend/src/pages/home/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <view>
+        <view>
+            <text style="font-size: 25px;color: #333;">
+            微博客首页
+            </text>
+        </view>
+        <view>
+            <text style="font-size: 18px;color: #999;">
+                {{errMsg}}
+            </text>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        onLoad(query) {
+            this.errMsg = query.errMsg || ''
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
